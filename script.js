7bf5fc5b749b9919d9a2bf146e67d1f66c90f0ef
ALD3X <dhainaut2002@gmail.com>
2023-06-21 23:26:11 +0200

    modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
-// Ce fichier est vide car l'animation est entièrement gérée en CSS.
-// Cependant, vous pouvez l'utiliser pour d'autres fonctionnalités JavaScript sur votre site.
+var loader = document.querySelector('.loader');
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+var posX = windowWidth / 2;
+var posY = windowHeight / 2;
+var directionX = 1;
+var directionY = 1;
+var speed = 1;
+var max = 20;
+
+loader.addEventListener('click', init_moveBall);
+function init_moveBall() {setInterval(moveBall, 10);}
+function moveBall() {
+  posX += directionX * speed;
+  posY += directionY * speed;
+  if (posX + loader.offsetWidth >= windowWidth || posX <= 0) {
+    directionX = -directionX;
+  }
+  
+  if (posY + loader.offsetHeight >= windowHeight || posY <= 0) {
+    directionY = -directionY;
+  }
+  loader.style.left = posX + 'px';
+  loader.style.top = posY + 'px';
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Space') {
+        if (max != 0){
+            increaseSpeed();
+            max --;
+        }
+    }
+});
+function increaseSpeed() {
+  speed *= 1.3;
+}
+
